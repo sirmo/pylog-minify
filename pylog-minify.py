191e488f45a33039908f2d12fdda9c9d130555bc
Mirsad <mirsad@gmail.com>
2012-06-16 22:36:41 -0400

    initial commit

---
+++ pylog-minify.py
@@ -0,0 +1,11 @@
+"""
+py log minify - This is a proof of concept. The simple idea is to statically code analyze python soruce code. And use the string formatting found in the code to 
+                compress logs beyond what's possible with conventional compression utilities. 
+                Goal being a dramatically reduced space requirement for long term log retention.
+
+Author: Mirsad Dedovic 
+"""
+
+
+if __name__ == '__main__':
+    pass
