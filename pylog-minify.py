4abfff5da8db2eec024f4a794d22e00a03833a2c
Mirsad <mirsad@gmail.com>
2012-06-16 22:47:02 -0400

    gitignored my virtualenv

--- pylog-minify.py
+++ pylog-minify.py
@@ -1,11 +1,13 @@
 """
-py log minify - This is a proof of concept. The simple idea is to statically code analyze python soruce code. And use the string formatting found in the code to 
+py log minify - This is a proof of concept. The simple idea is to statically code analyze python soruce code
+                and use the string formatting found in the code to 
                 compress logs beyond what's possible with conventional compression utilities. 
                 Goal being a dramatically reduced space requirement for long term log retention.
 
 Author: Mirsad Dedovic 
 """
 
+import OptionParser
 
 if __name__ == '__main__':
     pass
